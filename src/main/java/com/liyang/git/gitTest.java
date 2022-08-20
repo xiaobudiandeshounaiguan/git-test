b267d70a82f8fde82b782ed01d3116e550b08e05
liyang <2893818760@qq.com>
2022-08-20 13:23:09 +0800

    main commit2

--- src/main/java/com/liyang/git/gitTest.java
+++ src/main/java/com/liyang/git/gitTest.java
@@ -6,5 +6,6 @@ public class gitTest {
         System.out.println("Hello Git2.");
         System.out.println("Hello Git3.");
         System.out.println("hello git4 hot-fix");
+        System.out.println("main conflict");
     }
 }
