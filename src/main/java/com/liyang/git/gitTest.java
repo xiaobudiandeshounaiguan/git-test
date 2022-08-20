ce514faede6985d2e8993229d5466f9276b2af1a
liyang <2893818760@qq.com>
2022-08-20 13:21:22 +0800

    hot-fix commit2

--- src/main/java/com/liyang/git/gitTest.java
+++ src/main/java/com/liyang/git/gitTest.java
@@ -1,10 +1,13 @@
 package com.liyang.git;
 
+import com.sun.security.jgss.GSSUtil;
+
 public class gitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git.");
         System.out.println("Hello Git2.");
         System.out.println("Hello Git3.");
         System.out.println("hello git4 hot-fix");
+        System.out.println("hot-fix conflict");
     }
 }
