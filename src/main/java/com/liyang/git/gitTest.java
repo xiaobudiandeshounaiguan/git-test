4e8eda9ed3829833d63986e207d51b78495bdc7b
liyang <2893818760@qq.com>
2022-08-20 12:26:29 +0800

    second commit

--- src/main/java/com/liyang/git/gitTest.java
+++ src/main/java/com/liyang/git/gitTest.java
@@ -3,5 +3,6 @@ package com.liyang.git;
 public class gitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git.");
+        System.out.println("Hello Git2.");
     }
 }
